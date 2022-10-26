cddb47a889b972ee14cb67592e6269c364e97db3
Seth Adams <sadams65@uncc.edu>
2022-10-26 13:39:27 -0400

    updated try catch

--- player/player/Program.cs
+++ player/player/Program.cs
@@ -10,19 +10,24 @@ namespace HelloWorld
             String[] plyrName = new String[11]; // only 11 since that is how many are in a starting lineup
             int[] number = new int[11];
             String[] pos = new String[11];
+            bool result;
 
             for (int i = 0; i < plyrName.Length; i++)
             {
-                Console.WriteLine("Enter the player name: ");
-                plyrName[i] = Console.ReadLine();
-                Console.WriteLine("Enter the player number 1 - 99: ");
-                number[i] = int.Parse(Console.ReadLine());
-                if (number[i] > 99 || number[i]<0)
+                try
                 {
-                    Console.WriteLine("Outside of the boundry! Please enter a valid number within 1 - 99: ");
-                    number[i] = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the player name: ");
+                    plyrName[i] = Console.ReadLine();
+                    Console.WriteLine("Enter the player number 1 - 99: ");
+                    number[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("Error!");
                 }
             }
+
+            
 
             for (int j = 0; j < plyrName.Length; j++)
             {
